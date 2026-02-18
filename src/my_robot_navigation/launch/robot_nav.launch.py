b55f9362a1e69fedfa52b0ee6d8685b3036f0498
HarryK21 <harry.kengne@alumni.fh-aachen.de>
2026-02-18 09:43:09 +0100

    Changes on computer

--- src/my_robot_navigation/launch/robot_nav.launch.py
+++ src/my_robot_navigation/launch/robot_nav.launch.py
@@ -18,6 +18,7 @@ def generate_launch_description():
     # Create the launch configuration variables
     use_sim_time = LaunchConfiguration('use_sim_time')
     params_file = LaunchConfiguration('params_file')
+    default_bt_xml_filename = LaunchConfiguration('default_bt_xml_filename')
     autostart = LaunchConfiguration('autostart')
 
     # Launch configuration variables specific to simulation
@@ -27,13 +28,20 @@ def generate_launch_description():
     # Declare the launch arguments
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         'use_sim_time',
-        default_value='True',
+        default_value='false',
         description='Use simulation (Gazebo) clock if true')
 
     declare_params_file_cmd = DeclareLaunchArgument(
         'params_file',
         default_value=os.path.join(config_dir, 'nav2_params.yaml'),
         description='Full path to the ROS2 parameters file to use for all launched nodes')
+
+    declare_bt_xml_cmd = DeclareLaunchArgument(
+        'default_bt_xml_filename',
+        default_value=os.path.join(
+            get_package_share_directory('nav2_bt_navigator'),
+            'behavior_trees', 'navigate_w_replanning_and_recovery.xml'),
+        description='Full path to the behavior tree xml file to use')
 
     declare_autostart_cmd = DeclareLaunchArgument(
         'autostart', default_value='true',
@@ -60,6 +68,9 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(os.path.join(launch_dir, 'navigation_launch.py')),
         launch_arguments={'use_sim_time': use_sim_time,
                           'params_file': params_file,
+                          'default_bt_xml_filename': default_bt_xml_filename,
+                          'use_lifecycle_mgr': 'false',
+                          'map_subscribe_transient_local': 'true',
                           'autostart': autostart}.items())
 
     # Create the launch description and populate
@@ -68,13 +79,14 @@ def generate_launch_description():
     # Declare the launch options
     ld.add_action(declare_use_sim_time_cmd)
     ld.add_action(declare_params_file_cmd)
+    ld.add_action(declare_bt_xml_cmd)
     ld.add_action(declare_autostart_cmd)
 
     ld.add_action(declare_rviz_config_file_cmd)
     ld.add_action(declare_use_rviz_cmd)
 
     # Add the actions to launch all of the navigation nodes
-    ld.add_action(rviz_cmd)
+    #ld.add_action(rviz_cmd)
     ld.add_action(nav_cmd)
 
     return ld
